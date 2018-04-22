2198f5879393d28e92d3b92ac395e36878b76da8
osru <dfu3@njit.edu>
2018-04-22 02:28:44 -0400

    threshold alg tweak

--- libs.py
+++ libs.py
@@ -1,5 +1,8 @@
 #helper functions
 import os
 
+def convert(ampl, thres, upper, scale):
+    return int((ampl-thres)/(10.0/scale))
+
 def clr():
     os.system('clear')
--- procMic.py
+++ procMic.py
@@ -5,12 +5,10 @@ import numpy as np
 import os
 import pprint as pp
 
-np.set_printoptions(suppress=True) # don't use scientific notation
-
 CHUNK = 4096 # number of data points to read at a time
 RATE = 44100 # time resolution of the recording device (Hz)
 BASE = 900000 #initial sensitivity threshold
-SETUP_TIME = 100
+GROUPS = 8 #total number of frequency groups to visualize (starting at 64Hz)
 
 p=pyaudio.PyAudio() # start the PyAudio class
 stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
@@ -20,7 +18,8 @@ try:
     ampRange = set()
     spectrum = dict()
     high = 0
-
+    low = 0
+    
     clr()
     while(True):
         #read and process each sample
@@ -30,20 +29,23 @@ try:
         freq = np.fft.fftfreq(CHUNK,1.0/RATE)
         freq = freq[:int(len(freq)/2)] #nyquist
         
-        for i in range(8):
+        for i in range(GROUPS):
 
-            target = 2**(i+6)
-            thres = BASE-(i*100000)
+            target = (2**(i+6))
+            thres = BASE-int( ((i*100000)+((target/((GROUPS-i)*5))*100)) )
             ampl = fft[np.where(freq>target)[0][0]]
             
-            if(ampl >= thres):
-                if( ampl > high): high = int(ampl)
-                spectrum[target] = int(ampl)
+            if(ampl > thres):
+                cleaned = convert(ampl, thres, high, i+1)
+                if( cleaned > high): high = cleaned
+                if( cleaned < low): low = cleaned
+                spectrum[target] = cleaned
             else:
                 spectrum[target] = 0
 
         clr()
         pp.pprint(spectrum, width=1)
+        print('Low: {}\nHigh: {}'.format(low, high))
 
 except KeyboardInterrupt:
     stream.stop_stream()
