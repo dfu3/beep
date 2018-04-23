6ad94b427af862b59730eb9e18c28747f6e53d31
osru <dfu3@njit.edu>
2018-04-22 20:45:18 -0400

    cleanup dead code

--- procMic.py
+++ procMic.py
@@ -9,15 +9,12 @@ BAR_W = 2
 BAR_H = 100
 CHUNK = 4096 # number of data points to read at a time
 RATE = 44100 # time resolution of the recording device (Hz)
-BASE = 900000 #initial sensitivity threshold
-GROUPS = 8 #total number of frequency groups to visualize (starting at 64Hz)
 
 p=pyaudio.PyAudio() # start the PyAudio class
 stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
               frames_per_buffer=CHUNK) #uses default input device
 
 thresholds = [
-
     950000,#64
     875000,#128
     725000,#256
@@ -32,8 +29,6 @@ highs = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 try:
     ampRange = set()
     spectrum = dict()
-    high = 0
-    low = 0
     
     clr()
     while(True):
@@ -48,13 +43,11 @@ try:
         for i in range(len(thresholds)):
             target = (2**(i+6))
             thres = thresholds[i]
-            high = highs[i]
-            
             ampl = fft[np.where(freq>target)[0][0]]
-            if( ampl > high): high = ampl
+            if( ampl > highs[i]): highs[i] = ampl
             
             if(ampl > thres):
-                spectrum[target] = convert(ampl, high, thres)
+                spectrum[target] = convert(ampl, highs[i], thres)/2
             else:
                 spectrum[target] = 0
 
